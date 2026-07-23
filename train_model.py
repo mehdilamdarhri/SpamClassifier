b3407d8835da3397fa5bc0276b5d443e0c1e37de
Mehdi Lamdarhri <elmehdi021@gmail.com>
2026-07-23 17:43:15 +0100

    Added real SMS spam dataset

---
+++ train_model.py
@@ -0,0 +1,52 @@
+import pandas as pd
+from sklearn.model_selection import train_test_split
+from sklearn.feature_extraction.text import TfidfVectorizer
+from sklearn.naive_bayes import MultinomialNB
+from sklearn.metrics import accuracy_score
+
+# Convert file to csv
+#df = pd.read_csv("data/SMSSpamCollection", sep="\t", names=["label", "message"])
+#df.to_csv("data/spam.csv", index = False)
+
+df = pd.read_csv("data/spam.csv")
+#print(df.info())
+#print(df.isnull().sum())
+#print(df.head())
+print(df.shape)
+
+
+
+# Check the classes
+#print(df["label"].value_counts())
+
+# Convert ham spam into 0 and 1
+df["label"] = df["label"].map({
+    "ham": 0,
+    "spam": 1
+})
+
+
+
+# Splitting Data
+x = df["message"]
+y = df["label"]
+x_train, x_test, y_train, y_test = train_test_split(x, y, test_size = 0.2, random_state = 42)
+
+
+# Turning text into numbers
+vectorizer = TfidfVectorizer()
+
+# fit() = learn    transform() = Use what you learned => happens on training data
+x_train = vectorizer.fit_transform(x_train)
+# transform() on test data
+x_test = vectorizer.transform(x_test)
+
+
+# Use Naive Bayes model as a classifier
+model = MultinomialNB()
+model.fit(x_train, y_train)
+
+# Evaluate
+predictions = model.predict(x_test)
+accuracy = accuracy_score(predictions, y_test)
+print(accuracy)
