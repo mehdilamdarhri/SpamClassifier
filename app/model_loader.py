a2da08bd33edc0d7f9370b771df27a7962d44c00
Mehdi Lamdarhri <elmehdi021@gmail.com>
2026-08-02 18:03:16 +0100

    Add error handling and prepare application entry point

--- app/model_loader.py
+++ app/model_loader.py
@@ -1,9 +1,21 @@
 import joblib
+import pickle
+
 
 # Loads saved ML objects
 def load_model():
+    # Try to execute this code
+    try: 
     # The vectorizer is saved to transform future text inputs into the same numerical format the model was trained on.
-    model = joblib.load("models/spam_model.pkl")
-    vectorizer = joblib.load("models/tfidf_vectorizer.pkl")
+        model = joblib.load("models/spam_model.pkl")
+        vectorizer = joblib.load("models/tfidf_vectorizer.pkl")
 
-    return model, vectorizer
+        return model, vectorizer
+
+    # If a file is missing or if file exists but cannot be opened as a valid pickle file, run the code below    
+    except FileNotFoundError:
+        # Stop the program and report this error
+        raise FileNotFoundError("Could not load model files. Train the model first.")
+        
+    except  pickle.UnpicklingError:
+        raise pickle.UnpicklingError("The saved files are corrupted. Train the model again.")
--- app/predictor.py
+++ app/predictor.py
@@ -5,6 +5,9 @@ model, vectorizer = load_model()
 
 # Handles prediction logic
 def predict_message(message):
+    # message.strip() returns True if text exists and False if not. not True becomes False and the opposite
+    if not message.strip():
+        raise ValueError("Message cannot be empty")
     # vectorizer.transform expect a list and returns 2D list. we provide the outer brackets
     # The vectorizer creates the inner brackets => [[bracket for each messgae]]
     message_vector = vectorizer.transform([message])
